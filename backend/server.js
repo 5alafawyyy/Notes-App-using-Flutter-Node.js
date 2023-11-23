d49be09f04b315054545df652458c88ee30c516d
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-11-23 14:28:27 +0200

    Backend 2 - Connecting to MongoDB and using Postman

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
-// Initialization
-const express = require('express');
-const app = express();
+require('dotenv').config();
 
 
-// App Routes
-app.get('/', function (req, res) {
-    res.send('Hello from main route');
-});
+const mongoose = require('mongoose');
 
-app.get('/notes', function (req, res) {
-    res.send('This is the notes page');
-});
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+// Use them in your MongoDB connection string
+const connectionURL = `mongodb+srv://${username}:${password}@cluster0.fnv7hai.mongodb.net/notesdb`;
 
+// Connect to MongoDB
+mongoose.connect(connectionURL)
+    .then(() => {
+        console.log('Connected to MongoDB');
 
-// Strating Server on PORT 3000
-app.listen(3000, function () {
-    console.log("Server started at PORT: 3000");
-});
+        // App Routes
+        app.get('/', function (req, res) {
+            res.send('Hello from main route');
+        });
+
+        app.get('/notes', function (req, res) {
+            res.send('This is the notes page');
+        });
+
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err.message);
+    });
+
